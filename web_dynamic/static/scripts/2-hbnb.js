20ad350e18a5131c2b37c65588007b925c9d8c21
rachellmahang <rachellmahango@gmail.com>
2024-04-07 21:33:00 +0000

    web_dynamic

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+const $ = window.jQuery;
+
+$(document).ready(function () {
+  const amenities = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+    });
+    $('.amenities h4').html(Object.values(amenities).join(', ') || '&nbsp;');
+  });
+});
+
+$.get('http://localhost:5001/api/v1/status/')
+  .done(function (data, status) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  })
+  .fail(function () {
+    $('#api_status').removeClass('available');
+  });
